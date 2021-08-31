7559ea9c466bdbd5c1228dd9a78db9395a7901b1
Dharmik Savaliya <danny@Dharmiks-MacBook-Pro.local>
2021-08-31 11:18:48 -0700

    inital commit

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,16 @@
 // implement your posts router here
+const express = require('express')
+const router = express.Router()
+const Posts = require('./posts-model')
+
+router.get('/', (req, res) => {
+    Posts.find(req.query)
+    .then(post => {
+        res.status(200).json(post)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'The posts information could not be retrived'
+        })
+    })
+})
